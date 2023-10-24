93abb4b79eeb2f7af11e2a6e052a7e6075edf628
Rique <aguirree1985@gmail.com>
2023-10-24 14:26:45 -0700

    Adds clown emoji logic when click

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ const App = () => {
     "?"
   ])
   const handleGamePlay = (index) => {
-    alert(index)
+   // alert(index)
+   let updatedBoard = [...board]
+   updatedBoard[index] = "🤡"
+   setBoard(updatedBoard)
   }
   return (
     <>
